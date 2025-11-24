c719a51399fd3c03d7823b5ddc880bd861fed3b5
Sam <sam.fine@hotmail.co.uk>
2025-11-24 19:41:36 +0200

    final draft weather

--- APIs/weather_api_public/WeatherApiClient.py
+++ APIs/weather_api_public/WeatherApiClient.py
@@ -1,11 +1,9 @@
 import requests
-import csv
 import logging
-import pandas as pd
-# import numpy as np
 
-API_KEY = '8919aa67c51d4f48a1882658252011'
-URL = 'http://api.weatherapi.com/v1/current.json'
+
+class WeatherAPIURL:
+    URL = 'http://api.weatherapi.com/v1/current.json'
 
 class RelevantLocationData:
     '''
@@ -21,13 +19,15 @@ class RelevantLocationData:
 
 class RelevantCurrentData:
     LASTUPDATED = 'last_updated'
-    TEMPC = 'temp_c'
-    TEMPF = 'temp_f'
     CLOUD = 'cloud'
     WINDSPEED = 'wind_mph'
     HUMIDITY = 'humidity'
     PRESSURE = 'pressure_mb'
-    ALLCURRENTKEYS = [LASTUPDATED, TEMPC, TEMPF, CLOUD, WINDSPEED, HUMIDITY, PRESSURE]
+    ALLCURRENTKEYS = [LASTUPDATED, CLOUD, WINDSPEED, HUMIDITY, PRESSURE]
+    ALLCURRENTCSVCOLS = ['Last Updated', 'Clouds (%)', 'Wind Speed (mph)', 'Humidity (%)', 'Pressure (mb)']
+
+class TemperatureAPIFields:
+    temperature_mapping = {'c':'temp_c', 'f':'temp_f'}
 
 
 class WeatherAPIClient:
@@ -36,39 +36,55 @@ class WeatherAPIClient:
     '''
     def __init__(self, api_key):
         self.api_key = api_key
-        self.url = URL
-        self.logger = self.api_client_logger()
+        api_url = WeatherAPIURL()
+        self.url = api_url.URL
+        self.logger = self._setup_logger()
         self.RelevantLocationData = RelevantLocationData()
         self.RelevantCurrentData = RelevantCurrentData()
 
     def build_query(self, csv_row):
         '''
-        WeatherAPI normalises internally, so no need for consistent format in csv (e.g. capitalisation).
-        WeatherAPI expects at least city or zipcode.
-        The following are allowed combinations (in the order stated):
-            - Zip code alone
+        Defines the location string for the 'q' parameter in the API query.
+        All components must be separated by a comma (,).
+        The order listed is recommended for clarity, but the API's geocoding engine can often process valid components in any order.
+        WeatherAPI normalises internally, so format of string is irrelevant (e.g. capitalisation).
+        Query Structure: https://www.weatherapi.com/docs/
+        Region field is equivalent to state if available.
+        Data will be fetched only if the recommended location data has been provided.
+        NOTE: Threshold for input data can be altered.
+        NOTE: Returned data needs to be checked to ensure correct location has been fetched. eg Paris, FR does not return Paris
+
+        The following are RECOMMENDED/ALLOWED combinations for an unambiguous query and will return fetched data:
+            - Zip Code + Country - Best for international codes.
             - City + Country
             - City + State + Country
-            - City + State
-            - City alone
-        The following are not allowed combinations:
+
+        The following are AMBIGUOUS/NOT RECOMMENDED combinations and will not return fetched data:
+            - Zip Code alone - Generally works for common systems like US/UK but not specific enough for other countries.
+            - City alone - Likely returns an incorrect location due to multiple matches.
+            - City + State - Better, but lacks the necessary specificity of a country code.
+
+        The following are NOT ALLOWED combinations (Insufficient data for a specific point) and will not return fetched data:
             - Country only
             - State only
-            - State + Country without city
+            - State + Country without city/zip
+        """
         '''
-        original_data = {loc: csv_row[loc] for loc in self.RelevantLocationData.ALLORIGINALLOCATIONKEYS}
+        # strip whitespaces at end/ beginning for consistent query format
+        original_data = {loc: csv_row[loc].strip() for loc in self.RelevantLocationData.ALLORIGINALLOCATIONKEYS}
 
-        if original_data[self.RelevantLocationData.ZIPCODE]:
-            #self.logger.info(f'Zipcode {original_data[self.RelevantLocationData.ZIPCODE]} provided')
-            self.logger.info(f'CSV row data {original_data}')
-            return original_data[self.RelevantLocationData.ZIPCODE], original_data
-        if not original_data[self.RelevantLocationData.CITY]:
-            self.logger.info(f'Invalid csv row {original_data}: No zipcode or city')
+        if (original_data[self.RelevantLocationData.COUNTRY] and (original_data[self.RelevantLocationData.ZIPCODE] or
+                                                                  original_data[self.RelevantLocationData.CITY])):
+            self.logger.info(f'Valid CSV input row: {original_data}')
+            parts = [val for key, val in original_data.items() if val]
+            query =  ", ".join(parts).lower() # so same location but different cases are identified
+            self.logger.info(f'Query: {query}')
+            return query, original_data
+
+        else:
+            self.logger.info(f'Insufficient data in csv row {original_data}. \n Required: Country and (zipcode or city).')
             return None, original_data
 
-        parts = [x for x in (original_data[self.RelevantLocationData.CITY], original_data[self.RelevantLocationData.STATE], original_data[self.RelevantLocationData.COUNTRY]) if x]
-        self.logger.info(f'CSV row data {original_data}')
-        return ", ".join(parts), original_data
 
     def fetch_weather(self, query):
         '''
@@ -80,16 +96,21 @@ class WeatherAPIClient:
         }
 
         try:
-            response = requests.get(URL, params=params)
-            response.raise_for_status() # raises errors for 4xx (client error) or 5xx (server error)
-        except requests.exceptions.RequestException as e:
-            raise SystemExit(f'Error in API call {e}')
+            response = requests.get(self.url, params=params)
+            response.raise_for_status()
+        except Exception as e:
+            self.logger.error(f"API error for query '{query}': {e}")
+            return None, None  # signals failure and row remains unchanged
+        self.logger.info('Successful API response')
+
         return response.json(), query
 
-    def api_client_logger(self):
+    def _setup_logger(self):
         '''
-        Initiate logger for API.
+        Initialize and configure logger for API client.
         '''
-        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-        logger = logging.getLogger('WeatherAPIClient')
-        return logger
+        logging.basicConfig(
+            level=logging.INFO,
+            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        )
+        return logging.getLogger('WeatherAPIClient')
--- APIs/weather_api_public/weather_module.py
+++ APIs/weather_api_public/weather_module.py
@@ -1,23 +1,26 @@
-import requests
-import pandas as pd
-import numpy as np
+import argparse
+import logging
 import csv
 import os
+from datetime import datetime
+from WeatherApiClient import WeatherAPIClient, RelevantLocationData, RelevantCurrentData, TemperatureAPIFields
 
-from WeatherApiClient import WeatherAPIClient, RelevantLocationData, RelevantCurrentData
 
-API_KEY = '8919aa67c51d4f48a1882658252011'
+class FetchWeather:
 
-class WeatherModule:
-
-    def __init__(self, api_key, csv_path, output_path):
-        self.api_client = WeatherAPIClient(api_key)
-        self.logger = self.api_client.logger
-        self.csv_path = csv_path
-        self.output_path = output_path
+    def __init__(self):
+        self.args = self.args_parser()
+        self.csv_path = self.args.INPUT_PATH
+        self.output_path = self.args.SAVE_FILE_BASE_PATH
+        self.api_key = self.args.API_KEY
+        self.api_client = WeatherAPIClient(self.api_key)
+        self.temp_unit = self.args.temperature_unit
+        self.logger = self._setup_logger()
         self.RelevantLocationData = RelevantLocationData()
         self.RelevantCurrentData = RelevantCurrentData()
-        self.csv_columns = self.RelevantLocationData.ALLLOCATIONKEYS + self.RelevantCurrentData.ALLCURRENTKEYS # keeps order
+        self.TemperatureAPIFieldsMapping = TemperatureAPIFields.temperature_mapping
+        self.csv_columns = self.RelevantLocationData.ALLLOCATIONKEYS + self.RelevantCurrentData.ALLCURRENTCSVCOLS # keeps order
+        self.temperature_unit_column()
         self.cache = {}
 
     def main(self):
@@ -26,7 +29,8 @@ class WeatherModule:
         Use DictReader rather than pd.read_csv as we want an iterator to iterate through each row in the csv
         in case of large files to preserve memory (scalable).
         '''
-        output_path = os.path.join(self.output_path,'output_weather_file.csv')
+        now = datetime.now().strftime("%Y_%m_%dT%H_%M_%S")
+        output_path = os.path.join(self.output_path,f'output_weather_file_{now}.csv')
         with open(output_path, mode='w', newline='') as output_csv_file:
             writer = csv.DictWriter(output_csv_file, fieldnames=self.csv_columns)
 
@@ -36,12 +40,12 @@ class WeatherModule:
                 reader = csv.DictReader(csv_file)
                 for row in reader:
                     raw_data, original_data, query = self.fetch_data(row)
-                    if raw_data is None:
-                        writer.writerow(original_data)
-                        continue
-                    if raw_data is True:
+                    if query in self.cache:
                         parsed = self.cache[query]
                         self.logger.info(f'Repeated Location, take data from cache {parsed}')
+                    elif query is None: # invalid csv row or API response
+                        writer.writerow(original_data)
+                        continue
                     else:
                         parsed = self.parse_raw_data(raw_data, original_data, query)
                     writer.writerow(parsed)
@@ -52,13 +56,26 @@ class WeatherModule:
         Use client to fetch raw data from API.
         '''
         query, original_data = self.api_client.build_query(csv_row)
-        if query is None: # invalid csv data
+        if query is None or query in self.cache: # invalid csv data or in cache
             return None, original_data, query
-        if query in self.cache:
-            return True, original_data, query
 
         raw_data, query = self.api_client.fetch_weather(query)
+        if query is None: # failed API response
+            return None, original_data, query
         return raw_data, original_data, query
+
+    def temperature_unit_column(self):
+        '''
+        Identify from CLI arguments what temperature unit is required.
+        '''
+        self.temp_unit_lower = self.temp_unit.lower()
+        if self.temp_unit_lower  == "f":
+            self.temp_col = "Temperature (F)"
+        elif self.temp_unit_lower  == "k":
+            self.temp_col = "Temperature (K)"
+        else: # C default
+            self.temp_col = "Temperature (C)"
+        self.csv_columns.append(self.temp_col)
 
     def parse_raw_data(self, raw_data, original_data, query):
         '''
@@ -68,7 +85,7 @@ class WeatherModule:
         '''
         parsed_data = {}
         for location in original_data:
-            if location:
+            if original_data[location]:
                 parsed_data[location] = original_data[location]
             elif location == self.RelevantLocationData.COUNTRY:
                 parsed_data[location] = raw_data['location'][location]
@@ -76,23 +93,62 @@ class WeatherModule:
                 parsed_data[location] = raw_data['location']['name']
             elif location == self.RelevantLocationData.STATE:
                 parsed_data[location] = raw_data['location']['region']
+        # zipcode not provided in API response
 
         parsed_data[self.RelevantLocationData.LOCALTIME] = raw_data['location'][self.RelevantLocationData.LOCALTIME]
 
-        for current in self.RelevantCurrentData.ALLCURRENTKEYS:
-            parsed_data[current] = raw_data['current'][current]
+        if self.temp_unit_lower == 'k':
+            parsed_data[self.temp_col] = raw_data['current'][self.TemperatureAPIFieldsMapping['c']] + 273.15
+        else:
+            parsed_data[self.temp_col] = raw_data['current'][self.TemperatureAPIFieldsMapping[self.temp_unit_lower]]
 
+        for current, csv_col in zip(self.RelevantCurrentData.ALLCURRENTKEYS,self.RelevantCurrentData.ALLCURRENTCSVCOLS):
+            parsed_data[csv_col] = raw_data['current'][current]
+
+        # ensure variations of same query will be identified if repeated
         self.cache[query] = parsed_data
+        self.logger.info(f'Successfully parsed data')
 
         return parsed_data
 
-    # def reformat_result_headers(self):
-    #     '''
-    #     Current column headers are based off csv headers and API fields, change them to include units.
-    #     '''
-    #     self.csv_columns.rename({})
+    def _setup_logger(self):
+        '''
+        Initialize and configure logger for FetchWeather.
+        '''
+        logging.basicConfig(
+            level=logging.INFO,
+            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        )
+        return logging.getLogger('FetchWeather')
+
+
+    def args_parser(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-ip", "--input-path",
+                            dest="INPUT_PATH",
+                            help="Full path to input csv",
+                            default=r'C:\Users\samfi\OneDrive\Documents\Interviews_2025\Solid_data_team8\weather_data\example_input.csv')
+                            #required=True)
+        parser.add_argument("-sp", "--save-path",
+                            dest="SAVE_FILE_BASE_PATH",
+                            help="parent folder that will hold output files",
+                            default=r'C:\Users\samfi\OneDrive\Documents\Interviews_2025\Solid_data_team8\weather_data')
+                            #required=True)
+        parser.add_argument("-apik", "--api-key",
+                            dest="API_KEY",
+                            help="Personal API Key for Weather API",
+                            default=r'8919aa67c51d4f48a1882658252011')
+                            #required=True)
+        parser.add_argument("-tu", "--temp-unit",
+                            dest="temperature_unit",
+                            help="Decide which temperature unit to use",
+                            choices=['C','c', 'F', 'f', 'K', 'k'],
+                            default='C')
+
+        return parser.parse_args()
+
 
 
 if __name__ == '__main__':
-    Weather = WeatherModule(API_KEY,r'C:\Users\samfi\OneDrive\Documents\Interviews_2025\Solid_data_team8\weather_data/example_input.csv', r'C:\Users\samfi\OneDrive\Documents\Interviews_2025\Solid_data_team8\weather_data')
+    Weather = FetchWeather()
     Weather.main()
